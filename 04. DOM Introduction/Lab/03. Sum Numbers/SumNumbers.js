96163336ba0e245db67074950f09e631138b8a23
wolfie <yavorhristozov@gmail.com>
2022-05-15 09:36:26 +0300

    DOM Introduction

---
+++ 04. DOM Introduction/Lab/03. Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    const numberOne = Number(document.getElementById('num1').value);
+    const numberTwo = Number(document.getElementById('num2').value);
+
+    document.getElementById('sum').value = numberOne + numberTwo;
+}
