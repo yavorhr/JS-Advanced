135349f4316260e75848eba6b6c6888d12d24e85
wolfie <yavorhristozov@gmail.com>
2022-05-21 10:16:15 +0300

    Classes and attributes - Lab

---
+++ 08. Classes and Attributes/Lab/04. Point Distanc/PointDistance.js
@@ -0,0 +1,19 @@
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a, b) {
+    if (a instanceof Point == false || b instanceof Point == false) {
+      throw new TypeError('Parameter must be of type Point');
+    }
+    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+  }
+}
+
+const p1 = new Point(0, 0);
+const p2 = new Point(4, 3);
+
+console.log(p1, p2);
+console.log(Point.distance(p1, p2));
